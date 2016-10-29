f53ea3deef3be5a266152c6e7b87742e325b0929
John Pratt <jpratt9@gatech.edu>
2016-10-29 15:26:01 -0400

    made user, loan, load-offer schemas

--- model/loan/loan-schema.js
+++ model/loan/loan-schema.js
@@ -3,8 +3,13 @@ const Schema   = mongoose.Schema;
 
 
 const loanSchema = new Schema({
-  title: { type: String, required: true },
-  body:  { type: String }
+  loaner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  loanee: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  principle: { type: Number, required: true },
+  interest_rate: { type: Number, required: true },
+  period: { type: Number, required: true },
+  period_unit: { type: String, required: true, enum: ['day', 'month', 'year']},
+  payments: { type: Array }
 });
 
 
--- model/user/user-schema.js
+++ model/user/user-schema.js
@@ -3,8 +3,13 @@ const Schema   = mongoose.Schema;
 
 
 const userSchema = new Schema({
-  title: { type: String, required: true },
-  body:  { type: String }
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
+  bank_accounts: { type: Array },
+  email: { type: String, required: true },
+  encrypted_password: { type: String, required: true },
+  offers: { type: Array },
+  rating: { type: Number, required: true },
 });
 
 
