420e85b07c2d5c9a98bca226a6e0be83705b5946
John Pratt <jpratt9@gatech.edu>
2016-10-29 15:53:31 -0400

    loan schema in progress

--- model/loan/loan-schema.js
+++ model/loan/loan-schema.js
@@ -3,8 +3,14 @@ const Schema   = mongoose.Schema;
 
 
 const loanSchema = new Schema({
-  title: { type: String, required: true },
-  body:  { type: String }
+  loaner: { type: Schema.ObjectId, ref: 'User', required: true },
+  loanee: { type: Schema.ObjectId, ref: 'User', required: true },
+  principle: { type: Number, required: true },
+  interest_rate: { type: Number, min: 0, required: true },
+  minimum_payment: { type: Number, min: 0 },
+  regular_payment: { type: Number, min: 0, required: true },
+  period: { type: String, enum: ['day', 'week', 'month', 'year'], required: true},
+  transfers: { type: Array, required: true }
 });
 
 
